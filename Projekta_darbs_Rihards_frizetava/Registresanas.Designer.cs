43eab52b906d6ef13b8f7a0ee3871dc5eabfa72c
Rihards420 <rihards.sturitis3@gmail.com>
2024-04-20 20:08:03 +0300

    Dazadu funkciju izveide prieks parbaudes

--- Projekta_darbs_Rihards_frizetava/Registresanas.Designer.cs
+++ Projekta_darbs_Rihards_frizetava/Registresanas.Designer.cs
@@ -28,113 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.Vards_TTB = new System.Windows.Forms.MaskedTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.Uzvards_TTB = new System.Windows.Forms.MaskedTextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.E_pasts_TTB = new System.Windows.Forms.MaskedTextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.maskedTextBox4 = new System.Windows.Forms.MaskedTextBox();
+            this.Tel_nr_TTB = new System.Windows.Forms.MaskedTextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.maskedTextBox5 = new System.Windows.Forms.MaskedTextBox();
+            this.Parole_TTB = new System.Windows.Forms.MaskedTextBox();
+            this.Registresanas_poga = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.notirit_laukus = new System.Windows.Forms.Label();
+            this.bridinajums_label = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // maskedTextBox2
+            // Vards_TTB
             // 
-            this.maskedTextBox2.Location = new System.Drawing.Point(329, 128);
-            this.maskedTextBox2.Name = "maskedTextBox2";
-            this.maskedTextBox2.Size = new System.Drawing.Size(100, 20);
-            this.maskedTextBox2.TabIndex = 2;
+            this.Vards_TTB.Location = new System.Drawing.Point(101, 94);
+            this.Vards_TTB.Name = "Vards_TTB";
+            this.Vards_TTB.Size = new System.Drawing.Size(100, 20);
+            this.Vards_TTB.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(326, 112);
+            this.label1.Location = new System.Drawing.Point(98, 78);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(34, 13);
             this.label1.TabIndex = 5;
-            this.label1.Text = "label1";
+            this.label1.Text = "Vārds";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(326, 153);
+            this.label2.Location = new System.Drawing.Point(98, 119);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(46, 13);
             this.label2.TabIndex = 7;
-            this.label2.Text = "label2";
+            this.label2.Text = "Uzvārds";
             // 
-            // maskedTextBox1
+            // Uzvards_TTB
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(329, 169);
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
-            this.maskedTextBox1.TabIndex = 6;
+            this.Uzvards_TTB.Location = new System.Drawing.Point(101, 135);
+            this.Uzvards_TTB.Name = "Uzvards_TTB";
+            this.Uzvards_TTB.Size = new System.Drawing.Size(100, 20);
+            this.Uzvards_TTB.TabIndex = 6;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(326, 195);
+            this.label3.Location = new System.Drawing.Point(102, 161);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(42, 13);
             this.label3.TabIndex = 9;
-            this.label3.Text = "label3";
+            this.label3.Text = "E-pasts";
             // 
-            // maskedTextBox3
+            // E_pasts_TTB
             // 
-            this.maskedTextBox3.Location = new System.Drawing.Point(329, 211);
-            this.maskedTextBox3.Name = "maskedTextBox3";
-            this.maskedTextBox3.Size = new System.Drawing.Size(100, 20);
-            this.maskedTextBox3.TabIndex = 8;
+            this.E_pasts_TTB.Location = new System.Drawing.Point(101, 177);
+            this.E_pasts_TTB.Name = "E_pasts_TTB";
+            this.E_pasts_TTB.Size = new System.Drawing.Size(100, 20);
+            this.E_pasts_TTB.TabIndex = 8;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(326, 239);
+            this.label4.Location = new System.Drawing.Point(98, 205);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(86, 13);
             this.label4.TabIndex = 11;
-            this.label4.Text = "label4";
+            this.label4.Text = "Telefona numurs";
             // 
-            // maskedTextBox4
+            // Tel_nr_TTB
             // 
-            this.maskedTextBox4.Location = new System.Drawing.Point(329, 255);
-            this.maskedTextBox4.Name = "maskedTextBox4";
-            this.maskedTextBox4.Size = new System.Drawing.Size(100, 20);
-            this.maskedTextBox4.TabIndex = 10;
+            this.Tel_nr_TTB.Location = new System.Drawing.Point(101, 221);
+            this.Tel_nr_TTB.Name = "Tel_nr_TTB";
+            this.Tel_nr_TTB.Size = new System.Drawing.Size(100, 20);
+            this.Tel_nr_TTB.TabIndex = 10;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(326, 282);
+            this.label5.Location = new System.Drawing.Point(102, 248);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(37, 13);
             this.label5.TabIndex = 13;
-            this.label5.Text = "label5";
+            this.label5.Text = "Parole";
             // 
-            // maskedTextBox5
+            // Parole_TTB
             // 
-            this.maskedTextBox5.Location = new System.Drawing.Point(329, 298);
-            this.maskedTextBox5.Name = "maskedTextBox5";
-            this.maskedTextBox5.Size = new System.Drawing.Size(100, 20);
-            this.maskedTextBox5.TabIndex = 12;
+            this.Parole_TTB.Location = new System.Drawing.Point(101, 264);
+            this.Parole_TTB.Name = "Parole_TTB";
+            this.Parole_TTB.Size = new System.Drawing.Size(100, 20);
+            this.Parole_TTB.TabIndex = 12;
+            this.Parole_TTB.UseSystemPasswordChar = true;
+            // 
+            // Registresanas_poga
+            // 
+            this.Registresanas_poga.Location = new System.Drawing.Point(101, 321);
+            this.Registresanas_poga.Name = "Registresanas_poga";
+            this.Registresanas_poga.Size = new System.Drawing.Size(100, 23);
+            this.Registresanas_poga.TabIndex = 14;
+            this.Registresanas_poga.Text = "Reģistrēties";
+            this.Registresanas_poga.UseVisualStyleBackColor = true;
+            this.Registresanas_poga.Click += new System.EventHandler(this.Registresanas_poga_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(788, 282);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(8, 8);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(74, 32);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(163, 25);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Reģistrēšanās";
+            // 
+            // notirit_laukus
+            // 
+            this.notirit_laukus.AutoSize = true;
+            this.notirit_laukus.ForeColor = System.Drawing.Color.Blue;
+            this.notirit_laukus.Location = new System.Drawing.Point(131, 296);
+            this.notirit_laukus.Name = "notirit_laukus";
+            this.notirit_laukus.Size = new System.Drawing.Size(70, 13);
+            this.notirit_laukus.TabIndex = 17;
+            this.notirit_laukus.Text = "Notīrīt laukus";
+            this.notirit_laukus.Click += new System.EventHandler(this.notirit_laukus_Click);
+            // 
+            // bridinajums_label
+            // 
+            this.bridinajums_label.AutoSize = true;
+            this.bridinajums_label.ForeColor = System.Drawing.Color.Red;
+            this.bridinajums_label.Location = new System.Drawing.Point(98, 375);
+            this.bridinajums_label.Name = "bridinajums_label";
+            this.bridinajums_label.Size = new System.Drawing.Size(112, 13);
+            this.bridinajums_label.TabIndex = 18;
+            this.bridinajums_label.Text = "Aizpildiet visus laukus!";
+            this.bridinajums_label.Visible = false;
             // 
             // Registresanas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(310, 450);
+            this.Controls.Add(this.bridinajums_label);
+            this.Controls.Add(this.notirit_laukus);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Registresanas_poga);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.maskedTextBox5);
+            this.Controls.Add(this.Parole_TTB);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.maskedTextBox4);
+            this.Controls.Add(this.Tel_nr_TTB);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.maskedTextBox3);
+            this.Controls.Add(this.E_pasts_TTB);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.Uzvards_TTB);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.Vards_TTB);
             this.Name = "Registresanas";
             this.Text = "Registresanas";
             this.Load += new System.EventHandler(this.Registresanas_Load);
@@ -144,15 +206,20 @@
         }
 
         #endregion
-        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.MaskedTextBox Vards_TTB;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox Uzvards_TTB;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.MaskedTextBox E_pasts_TTB;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox4;
+        private System.Windows.Forms.MaskedTextBox Tel_nr_TTB;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox5;
+        private System.Windows.Forms.MaskedTextBox Parole_TTB;
+        private System.Windows.Forms.Button Registresanas_poga;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label notirit_laukus;
+        private System.Windows.Forms.Label bridinajums_label;
     }
 }
